70fccb9c3b7f455581883924b1abe5033e5549e5
Kaya <kayalion@kayalion.net>
2016-04-01 12:38:42 +0200

    added end-of-day age

--- src/ride/web/cms/controller/backend/action/node/VarnishNodeAction.php
+++ src/ride/web/cms/controller/backend/action/node/VarnishNodeAction.php
@@ -3,7 +3,7 @@
 namespace ride\web\cms\controller\backend\action\node;
 
 use ride\library\cms\node\Node;
-use ride\library\i18n\I18n;
+use ride\library\i18n\translator\Translator;
 use ride\library\validation\exception\ValidationException;
 
 use ride\service\CmsVarnishService;
@@ -26,6 +26,10 @@ class VarnishNodeAction extends AbstractNodeAction {
      */
     const ROUTE = 'cms.node.varnish';
 
+    /**
+     * Available cache times
+     * @var array
+     */
     private $times = array(
         60, // 1 minute
         300, // 5 minutes
@@ -51,15 +55,9 @@ class VarnishNodeAction extends AbstractNodeAction {
             return;
         }
 
+        $this->setContentLocale($locale);
         $cms->setLastAction(self::NAME);
 
-        $url = $site->getBaseUrl($locale);
-        if (!$url) {
-            $url = $this->request->getBaseUrl();
-        }
-
-        $cache = $node->get('cache.target', 'inherit', false);
-        $translator = $this->getTranslator();
         $referer = $this->request->getQueryParameter('referer');
         if (!$referer) {
             $referer = $this->getUrl('cms.node.varnish', array(
@@ -70,16 +68,17 @@ class VarnishNodeAction extends AbstractNodeAction {
             ));
         }
 
-        // Set data
+        $translator = $this->getTranslator();
+
+        $cache = $node->get('cache.target', 'inherit', false);
         $data = array(
             'cacheTarget' => $cache,
-            'sharedMaxAge' => in_array($cache, ['intermediate', 'all']) ? $node->getHeader($locale, 's-maxage') : null,
+            'sharedMaxAge' => in_array($cache, array('intermediate', 'all')) ? $node->getHeader($locale, 's-maxage') : null,
             'maxAge' => $cache == 'all' ? $node->getHeader($locale, 'max-age') : ($cache == 'intermediate' ? 0 : null),
         );
 
         $formHeaders = $this->createFormBuilder($data);
         $formHeaders->setAction('headers');
-
         $formHeaders->addRow('cacheTarget', 'option', array(
             'label' => $translator->translate('label.cache.target'),
             'options' => $this->getCacheOptions($node, $translator, $locale),
@@ -90,20 +89,18 @@ class VarnishNodeAction extends AbstractNodeAction {
                 'required' => array(),
             ),
         ));
-
         $formHeaders->addRow('maxAge', 'select', array(
             'label' => $translator->translate('label.header.maxage'),
             'description' => $translator->translate('label.header.maxage.description'),
-            'options' => $this->getTimeOptions(0, 3600, $translator),
+            'options' => $this->getTimeOptions($translator, 0, 3600),
             'attributes' => array(
                 'class' => 'option-cachetarget option-cachetarget-all',
             ),
         ));
-
         $formHeaders->addRow('sharedMaxAge', 'select', array(
             'label' => $translator->translate('label.header.smaxage'),
             'description' => $translator->translate('label.header.smaxage.description'),
-            'options' => $this->getTimeOptions(0, 31536000, $translator),
+            'options' => $this->getTimeOptions($translator, 0, 31536000),
             'attributes' => array(
                 'class' => 'option-cachetarget option-cachetarget-intermediate option-cachetarget-all',
             ),
@@ -117,10 +114,9 @@ class VarnishNodeAction extends AbstractNodeAction {
                 $data = $formHeaders->getData();
 
                 $node->set('cache.target', $data['cacheTarget']);
-
                 $node->setHeader($locale, 'max-age', $data['cacheTarget'] == 'all' ? $data['maxAge'] : ($data['cacheTarget'] == 'intermediate' ? 0 : ($data['cacheTarget'] == 'inherit' ? null : '')));
                 $node->setHeader($locale, 's-maxage', in_array($data['cacheTarget'], ['intermediate', 'all']) ? $data['sharedMaxAge'] : ($data['cacheTarget'] == 'inherit' ? null : ''));
-                $node->setHeader($locale, 'Expires', 'Thu, 05 Apr 1984 18:00:00 GMT');
+                $node->setHeader($locale, 'Expires', 'Wed, 06 Jul 1983 5:00:00 GMT');
 
                 $cms->saveNode($node, "Set cache properties for " . $node->getName());
 
@@ -196,7 +192,7 @@ class VarnishNodeAction extends AbstractNodeAction {
      * @return array Array with the cache code as key and the translation as
      * value
      */
-    protected function getCacheOptions(Node $node, $translator, $locale) {
+    protected function getCacheOptions(Node $node, Translator $translator, $locale) {
         $options = array();
 
         $parentNode = $node->getParentNode();
@@ -219,7 +215,7 @@ class VarnishNodeAction extends AbstractNodeAction {
      *
      * @return string
      */
-    protected function getInheritedCacheOption( Node $parentNode, $translator, $locale) {
+    protected function getInheritedCacheOption(Node $parentNode, Translator $translator, $locale) {
         $value = $parentNode->get('cache.target', null, true, true);
         $maxAge = $parentNode->getHeader($locale, 'max-age');
         $sharedMaxAge = $parentNode->getHeader($locale, 's-maxage');
@@ -239,21 +235,25 @@ class VarnishNodeAction extends AbstractNodeAction {
 
     /**
      * Gets the time options between a given range
-     * @param $min
-     * @param $max
-     * @param $translator
-     *
+     * @param \ride\library\i18n\translator\Translator $translator
+     * @param integer $min
+     * @param integer $max
      * @return array
      */
-    protected function getTimeOptions($min, $max, $translator) {
+    protected function getTimeOptions(Translator $translator, $min, $max) {
         $options = array();
 
-        foreach($this->times as $time) {
-            if($time >= $min && $time <= $max) {
+        foreach ($this->times as $time) {
+            if ($time >= $min && $time <= $max) {
                 $options[$time] = $translator->translate('label.cache.time.' . $time);
             }
         }
 
+        if ($min < 86400 && 86400 < $max) {
+            $options['end-of-day'] = $translator->translate('label.cache.time.end.day');
+        }
+
         return $options;
     }
+
 }
